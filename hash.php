f7d56744abb1dd09eda760076734295798d879a4
ananthsridev12 <m.ananthsridev@gmail.com>
2026-02-20 12:41:57 +0530

    Fix merge-conflict markers causing HTTP 500

--- hash.php
+++ hash.php
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 <?php
 echo password_hash('Sridev@12', PASSWORD_BCRYPT);
-=======
-<?php
-echo password_hash('Sridev@12', PASSWORD_BCRYPT);
->>>>>>> f114d65 (Add familytree project files)
